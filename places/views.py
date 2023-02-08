ff507d139d9e5a00e261ef94b2c04b795e187c81
Katsutami7moto <nclsf87@gmail.com>
2023-02-08 15:11:53 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,4 +1,36 @@
 from django.shortcuts import render
 
+
+places = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [37.62, 55.793676]
+            },
+            "properties": {
+                "title": "«Легенды Москвы",
+                "placeId": "moscow_legends",
+                "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [37.64, 55.753676]
+            },
+            "properties": {
+                "title": "Крыши24.рф",
+                "placeId": "roofs24",
+                "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json"
+            }
+        }
+    ]
+}
+
+
 def start(request):
-    return render(request, 'start.html')
+    return render(request, 'start.html', {'places_geojson': places})
